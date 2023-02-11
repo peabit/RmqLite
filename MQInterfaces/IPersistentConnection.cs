fff00d3299de01913b93455a1d27e82a68a734fd
dmitry.zarechnev@gmail.com <dmitry.zarechnev@gmail.com>
2023-02-11 17:13:11 +0300

    Add RMQ interfaces

---
+++ MQInterfaces/IPersistentConnection.cs
@@ -0,0 +1,10 @@
+﻿using RabbitMQ.Client;
+
+namespace Rmq.Interfaces;
+
+public interface IPersistentConnection : IDisposable
+{
+    bool IsConnected { get; }
+    bool TryConnect();
+    IModel CreateModel();
+}
---
+++ MQInterfaces/IPublisher.cs
@@ -0,0 +1,6 @@
+﻿namespace Rmq.Interfaces;
+
+public interface IPublisher
+{
+    void Publish<TMessage>(TMessage message);
+}
